ae2b6f6779b318abd7822f58cecd3b7f93908394
Chad Christensen <chadchristensen2@gmail.com>
2019-10-15 14:02:03 -0400

    Fix errors due to merge

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,118 +1,100 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GameBoard from "./GameBoard";
-import "bootstrap/dist/css/bootstrap.css";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GameBoard from './GameBoard';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends React.Component {
   state = {
-    squares: [[0, 0, 0], [0, 0, 0], [0, 0, 0 ]],
+    squares: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
     selectedArray: 0,
-    isXturn: true,
+    isXturn: true
   };
 
-  selectArray = (rowIndex) => {
+  selectArray = rowIndex => {
     this.setState({
       selectedArray: rowIndex
-    })
-    // console.log(this.state.selectedArray)
+    });
   };
-
 
   handlePushClick = () => {
     let player = this.state.isXturn ? 'X' : 'O';
-    if(this.state.isXturn){
-      player="X"
-    } else{
-      player = "O"
-    };
+
     let newSquares = [...this.state.squares];
     let newRow = [...this.state.squares[this.state.selectedArray]];
-    if(newRow.includes(0)){
+    if (newRow.includes(0)) {
       newRow[newRow.indexOf(0)] = player;
     } else {
       alert('Selected array full, cannot push to selected array.');
     }
-    newSquares[this.state.selectedArray]=newRow;
+    newSquares[this.state.selectedArray] = newRow;
     this.setState({
-      squares: newSquares, isXturn: !this.state.isXturn
+      squares: newSquares,
+      isXturn: !this.state.isXturn
     });
   };
 
   handlePopClick = () => {
     let newSquares = [...this.state.squares];
     let newRow = [...this.state.squares[this.state.selectedArray]];
-    if(!newRow[0]){
-        alert('please choose another array to pop')
-    } else if( newRow.includes(0) ){
-        newRow[newRow.indexOf(0)-1] = 0
+    if (!newRow[0]) {
+      alert('please choose another array to pop');
+    } else if (newRow.includes(0)) {
+      newRow[newRow.indexOf(0) - 1] = 0;
     } else {
-        newRow[newRow.length - 1] = 0
+      newRow[newRow.length - 1] = 0;
     }
     newSquares[this.state.selectedArray] = newRow;
     this.setState({
-        squares: newSquares 
+      squares: newSquares
     });
   };
 
-    handleUnshiftClick = () => {
-        let player = this.state.isXturn ? 'X' : 'O';
-        if(this.state.isXturn){
-            player="X"
-        } else {
-            player = "O"
-        };
-        let newSquares = [...this.state.squares];
-        let newRow = [...this.state.squares[this.state.selectedArray]];
-        if(newRow[2]){alert('please choose another array to unshift')};
-        newRow.pop();
-        newRow.unshift(player);
-        newSquares[this.state.selectedArray] = newRow;
-        this.setState({
-          squares: newSquares 
-         })
-    };
-    
+  handleUnshiftClick = () => {
+    let player = this.state.isXturn ? 'X' : 'O';
 
-    handleShiftClick = () => {
-        let newSquares = [...this.state.squares];
-        let newRow = [...this.state.squares[this.state.selectedArray]];
-        if(!newRow[0]){
-            alert('please choose another array to shift')
-        } else{
-            newRow.shift();
-            newRow.push(0);
-        }
-        newSquares[this.state.selectedArray] = newRow;
-        this.setState({
-            squares: newSquares 
-        });
+    let newSquares = [...this.state.squares];
+    let newRow = [...this.state.squares[this.state.selectedArray]];
+    if (newRow[2]) {
+      alert('please choose another array to unshift');
     }
+    newRow.pop();
+    newRow.unshift(player);
+    newSquares[this.state.selectedArray] = newRow;
+    this.setState({
+      squares: newSquares
+    });
+  };
 
+  handleShiftClick = () => {
+    let newSquares = [...this.state.squares];
+    let newRow = [...this.state.squares[this.state.selectedArray]];
+    if (!newRow[0]) {
+      alert('please choose another array to shift');
+    } else {
+      newRow.shift();
+      newRow.push(0);
+    }
+    newSquares[this.state.selectedArray] = newRow;
+    this.setState({
+      squares: newSquares
+    });
+  };
 
   render() {
-
-      let player = this.state.isXturn ? 'X' : 'O';
-      if(this.state.isXturn){
-          player = 'X'
-      }else{
-         player = 'O'
-      }
+    let player = this.state.isXturn ? 'X' : 'O';
 
     return (
       <div className="App">
-        <GameBoard 
-        squares={this.state.squares} 
-        selectArray={this.selectArray}
+        <GameBoard
+          squares={this.state.squares}
+          selectArray={this.selectArray}
         />
-        <div className='buttons' >
-        <button onClick={this.handlePushClick}>Push</button>
-        <button onClick={this.handlePopClick} >Pop</button>
-        <button onClick={this.handleUnshiftClick} >Unshift</button>
-        <button onClick={this.handleShiftClick} >Shift</button>
-
-        {player}'s turn
-
+        <div className="buttons">
+          <button onClick={this.handlePushClick}>Push</button>
+          <button onClick={this.handlePopClick}>Pop</button>
+          <button onClick={this.handleUnshiftClick}>Unshift</button>
+          <button onClick={this.handleShiftClick}>Shift</button>
+          {player}'s turn
         </div>
       </div>
     );
@@ -121,7 +103,7 @@ class App extends React.Component {
 
 // ========================================
 
-export default App
+export default App;
 
 function calculateWinner(squares) {
   const lines = [
--- client/src/components/GameBoard.js
+++ client/src/components/GameBoard.js
@@ -1,56 +1,25 @@
-import React from "react";
-import GameRow from "./GameRow";
+import React from 'react';
+import GameRow from './GameRow';
 
 class Board extends React.Component {
   render() {
     return (
       <div>
-        <div
-          onClick={() => {
-            this.props.selectArray(0);
-          }}
-        >
-          <GameRow
-            className='board-row'
-            squares={this.props.squares[0]}
-            rowIndex={0}
-          />
-        </div>
-
-        <div
-          onClick={() => {
-            this.props.selectArray(1);
-          }}
-        >
-          <GameRow
-            className='board-row'
-            squares={this.props.squares[1]}
-            rowIndex={1}
-          />
-        </div>
-
-        <div
-          onClick={() => {
-            this.props.selectArray(2);
-          }}
-        >
-          <GameRow
-            className='board-row'
-            squares={this.props.squares[2]}
-            rowIndex={2}
-          />
         {this.props.squares.map((row, index) => {
-          <div
-            onClick={() => {
-              this.props.selectArray(index)
-            }}
-          >
-           <GameRow
-               className='board-row'
-               squares={this.props.squares[index]}
-               rowIndex={index}
-           />
-         </div>
+          return (
+            <div
+              onClick={() => {
+                this.props.selectArray(index);
+              }}
+            >
+              <GameRow
+                className="board-row"
+                squares={this.props.squares[index]}
+                rowIndex={index}
+              />
+            </div>
+          );
+        })}
       </div>
     );
   }
